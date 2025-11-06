a73034dfa2706199cf17ee941d257a6724f10366
muhammmadhashim-rgb <muhammadhashim4412@gmail.com>
2025-11-06 05:08:39 +0500

    Route set

---
+++ src/Pages/Shop page/Navbar.jsx
@@ -0,0 +1,12 @@
+import React, { Component } from 'react'
+import Navbar from '../../repetitiveComponents/Navbar'
+
+export default class navbar extends Component {
+  render() {
+    return (
+      <div>
+        <Navbar></Navbar>
+      </div>
+    )
+  }
+}
